63151b0d30294d2b92b14a5292836e04572ad648
Kkyk222 <keith.khor.yk@gmail.com>
2021-11-28 06:46:10 +0000

    entry

--- src/lib/Entry.svelte
+++ src/lib/Entry.svelte
@@ -1,4 +1,4 @@
-<button type="button" class="list-group-item list-group-item-action">
+<button type="button" class="list-group-item list-group-item-action text-sucess">
 	<div class="d-flex w-100 justify-content-between lh-1">
     	<h6 class="my-0">1 Nov 2021</h6>
     	<span class="text-success">Best</span>
--- src/lib/Navbar.svelte
+++ src/lib/Navbar.svelte
@@ -1,4 +1,4 @@
-<nav class="navbar navbar-light bg-light">
+<nav class="navbar navbar-light bg-secondary">
     <div class="container-fluid">
       <a class="navbar-brand" href="/">Keith Mood Tracker</a>
     </div>
