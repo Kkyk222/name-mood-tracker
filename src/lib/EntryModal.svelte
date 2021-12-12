068f7b33441bd46ff1a151cc576eec4d2fa516d8
Kkyk222 <keith.khor.yk@gmail.com>
2021-12-12 07:41:10 +0000

    vercel

--- src/lib/EntryModal.svelte
+++ src/lib/EntryModal.svelte
@@ -1,6 +1,33 @@
 <script>
     let emoji = "😐";
-    let emojiList = "worst: '😭', bad: '🙁', okay: '😐', good: '🙂',best: '😁'"
+    let emojiList = {worst: '😭', bad: '🙁', okay: '😐', good: '🙂',best: '😁'};
+    let day = '1'
+    let month = '1'
+    let year = '2021'
+    let mood = 'Okay'
+    let comment = 'This is a comment'
+
+import supabase from '$lib/db';
+
+// Insert entry
+async function saveEntry() {
+    const { error } = await supabase.from('moodEntries').insert(
+   	 {
+   		 user_id: supabase.auth.user().id,
+   		 day: day,
+   		 month: month,
+   		 year: year,
+   		 mood: mood,
+   		 comment: comment
+   	 }
+    );
+    if (error) alert(error.message);
+
+    location.reload(); // Refresh the page.
+}
+
+
+
 </script>
 <div class="modal fade" id="newEntry" tabindex="-1">
     <div class="modal-dialog modal-dialog-centered modal-dialog-scrollable">
@@ -16,19 +43,19 @@
 <div class="row">
     <div class="col">
    	 <form class="form-floating">
-   		 <input type="number" class="form-control" id="dayInput" value="1" min="1" max="31"/>
+   		 <input type="number" class="form-control" id="dayInput" bind:value={day} min="1" max="31"/>
    		 <label for="dayInput">Day</label>
    	 </form>
     </div>
     <div class="col">
    	 <form class="form-floating">
-   		 <input type="number" class="form-control" id="monthInput" value="1" min="1" max="12"/>
+   		 <input type="number" class="form-control" id="monthInput" bind:value={month} min="1" max="12"/>
    		 <label for="monthInput">Month</label>
    	 </form>
     </div>
     <div class="col">
    	 <form class="form-floating">
-   		 <input type="number" class="form-control" id="yearInput" value="2021" min="2021" />
+   		 <input type="number" class="form-control" id="yearInput" bind:value={year} min="2021" />
    		 <label for="yearInput">Year</label>
    	 </form>
     </div>
@@ -57,6 +84,7 @@ id="worst"
 autocomplete="off"
 on:click={() => {
     emoji = emojiList.worst;
+    mood = 'Worst';
 }}
 />
 <label class="btn btn-outline-danger" for="worst">Worst</label>
@@ -70,6 +98,7 @@ on:click={() => {
     autocomplete="off"
     on:click={() => {
         emoji = emojiList.bad;
+        mood = 'Bad';
     }}
 />
 <label class="btn btn-outline-warning" for="bad">Bad</label>
@@ -82,6 +111,7 @@ on:click={() => {
     autocomplete="off"
     on:click={() => {
         emoji = emojiList.okay;
+        mood = 'Okay';
     }}
 />
 <label class="btn btn-outline-primary" for="okay">Okay</label>
@@ -94,6 +124,7 @@ on:click={() => {
     autocomplete="off"
     on:click={() => {
         emoji = emojiList.good;
+        mood = 'Good';
     }}
 />
 <label class="btn btn-outline-info" for="good">Good</label>
@@ -107,14 +138,11 @@ on:click={() => {
     autocomplete="off"
     on:click={() => {
         emoji = emojiList.best;
+        mood = 'Best';
     }}
 />
-<label class="btn btn-outline-sucess" for="best">Best</label>
-<!-- Comments -->
-<div class="form-floating">
-    <textarea class="form-control" id="comment" />
-    <label for="commentTextarea">Comments</label>
-</div>
+<label class="btn btn-outline-success" for="best">Best</label>
+
 
 
 
@@ -122,6 +150,12 @@ on:click={() => {
 
 
    </div>
+   <!-- Comments -->
+<div class="form-floating">
+    <textarea class="form-control" bind:value={comment
+    } id="comment" />
+    <label for="commentTextarea">Comments</label>
+</div>
 </div>
 
 
@@ -129,7 +163,7 @@ on:click={() => {
 
    		 <div class="modal-footer">
    			 <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Cancel</button>
-   			 <button type="button" class="btn btn-primary">Add</button>
+   			 <button type="button" class="btn btn-primary" on:click={saveEntry}>Add</button>
    		 </div>
    	 </div>
     </div>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,9 +1,28 @@
 <!-- Entries -->
 <script>
+    import supabase from '$lib/db';
     import Greeting from "$lib/Greeting.svelte";
     import Entry from "$lib/Entry.svelte";
     import EntryModal from "$lib/EntryModal.svelte";
+    async function signOut(){
+        const{error}=await supabase.auth.signOut({
+            email
+        });
+        if(error)alert(error.message);
+    }
+    
+
+// Select entries
+async function getEntries() {
+    const { data, error } = await supabase.from('moodEntries').select();
+    if (error) alert(error.message);
+
+    return data;
+}
+
 </script>
+<!-- Sign Out -->
+
 <Greeting />
 <EntryModal />
 <section class="container px-4 py-3">
@@ -14,8 +33,25 @@
 
     <div class="list-group mb-3">
    	 <!-- Individual Entries -->
-    	<Entry />
+    	<!-- Individual Entries -->
+{#await getEntries()}
+<p>Fetching data...</p>
+{:then data}
+{#each data as entry}
+    <Entry
+        date={entry.day + '-' + entry.month + '-' + entry.year}
+        mood={entry.mood}
+        comment={entry.comment}
+    />
+{/each}
+{:catch error}
+<p>Something went wrong while fetching the data:</p>
+<pre>{error}</pre>
+{/await}
         
    	 
     </div>
+</section>
+<section class="container px-4 py-3 text-center">
+    <button class="btn btn-secondary" on:click={signOut}>Logout</button>
 </section>
